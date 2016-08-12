48a69e322cbbcdb51692b4169872c27eb22b21e9
wanghao <wanghao@csdn.net>
2016-08-12 17:35:06 +0800

    Part III: Distributing MapReduce tasks

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
@@ -17,12 +20,35 @@ func (mr *Master) schedule(phase jobPhase) {
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
 
-	// All ntasks tasks have to be scheduled on workers, and only once all of
-	// them have been completed successfully should the function return.
-	// Remember that workers may fail, and that any given worker may finish
-	// multiple tasks.
-	//
-	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-	//
+	var wg sync.WaitGroup
+	for i :=0; i < ntasks; i++{
+		wg.Add(1)
+
+		go func(_taskNum int, _nios int, _phase jobPhase){
+			debug("DEBUG: current taskNum: %v, nios: %v, phase: %v\n", _taskNum, _nios, _phase)
+			defer wg.Done()
+
+			for {
+				worker := <-mr.registerChannel
+
+				var args DoTaskArgs
+				args.File = mr.files[_taskNum]
+				args.JobName = mr.jobName
+				args.NumOtherPhase = _nios
+				args.Phase = _phase
+				args.TaskNumber = _taskNum
+
+				ok := call(worker, "Worker.DoTask", &args, new(struct{}))
+				if ok {
+					go func() {
+						mr.registerChannel <- worker
+					}()
+					break
+				}
+			}
+		}(i, nios, phase)
+	}
+	wg.Wait()
+
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
